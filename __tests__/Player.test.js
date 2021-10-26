d23787feed7ae97e8ccca8c8c37c20735c854dd1
Victor Smith <victorj500@gmail.com>
2021-10-26 17:16:37 -0500

    removed node_modules

---
+++ __tests__/Player.test.js
@@ -0,0 +1,19 @@
+// imports th Potion() constructor into the test, establishing Potion asw a useable variable
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+const Player = require('../lib/Player');
+
+test('creates a player object', () => {
+  const player = new Player('Dave');
+
+  expect(player.name).toBe('Dave');
+  expect(player.health).toEqual(expect.any(Number));
+  expect(player.strength).toEqual(expect.any(Number));
+  expect(player.agility).toEqual(expect.any(Number));
+
+  expect(player.inventory).toEqual(
+    expect.arrayContaining([expect.any(Object)])
+  );
+})
